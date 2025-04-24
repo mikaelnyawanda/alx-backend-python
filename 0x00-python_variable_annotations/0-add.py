906dc4042eec069563f3b7966991ee5960d81ca3
mikaelnyawanda <bolomycke65@gmail.com>
2025-04-24 19:17:00 +0000

    0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for a simple add function with type annotations."""
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of two float numbers."""
+    return a + b
+
